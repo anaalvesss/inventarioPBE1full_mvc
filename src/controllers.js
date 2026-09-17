dda7ffb37f8eb1b678aac72e2cbd009dfc98bcc5
anaalveess <anaalvssss.6@gmail.com>
2026-09-17 10:52:44 -0300

    💋

--- src/controllers.js
+++ src/controllers.js
@@ -1,18 +1,20 @@
-const dados = require("../dados.json")
+const bens = require("../dados.json")
 
-function autoIncrement(){
-    const ultimoId =  Number(dados[dados.length - 1].id)
+function autoIncremet(){
+    const ultimoId = Number(bens[bens.length - 1].id)
     return ultimoId + 1;
 }
 
-const create = (req, res) =>{
-    const dados = read.body
-    dados.add(dados)
+//CRUDS
+const create = (req, res)=>{
+    const dados = req.body
+    dados.id = autoIncremet()
+    bens.push(dados)
     res.status(201).json(dados)
 }
 
-const read = (req, res) => {
-    res.json(dados)
+const read = (req, res)=>{
+    res.json(bens)
 }
 
 module.exports = {
